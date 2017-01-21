2d1b7eafefecad9a0ed4949cd4d2d91bfc505391
DESKTOP-JU9CP81\Ray <raymondw.phillips@yahoo.com>
2017-01-21 01:46:00 -0600

    no message

---
+++ Unity Project/Assets/Scripts/PulseCannon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PulseCannon : MonoBehaviour {
+    public float Range = 1;
+    public float Force = 10;
+    private bool firing = false;
+   
+  	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        firing = Input.GetButton("Fire1");
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (firing)
+        { 
+            var direction = (other.transform.position - transform.position).normalized;
+            direction = new Vector3(direction.x, 0, direction.z);
+            var distance = Vector3.Distance(other.transform.position, transform.position);
+            other.GetComponent<Rigidbody>().AddForce(direction * Force);
+        }
+    }
+}
