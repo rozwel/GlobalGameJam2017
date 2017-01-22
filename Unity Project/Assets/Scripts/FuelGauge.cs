10e52b289ad08fb3f48ea72d558ecdbe3ca80e3c
Sarah R <phoesarah@hotmail.com>
2017-01-21 20:15:13 -0600

    Added new Fuel Guage not quite working

---
+++ Unity Project/Assets/Scripts/FuelGauge.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelGauge : MonoBehaviour
+{
+    [SerializeField]
+    private float fillAmount;
+    [SerializeField]
+    private float fillAmountBlack;
+
+    public float MaxFuelAmt { get; set; }
+    public float NewFuelAmt
+    {
+        set
+        {
+            fillAmount = Map(value, MaxFuelAmt);
+            //fillAmount = Map(NewFuelAmt, MaxFuelAmt);
+            fillAmountBlack = fillAmount + (float).1;
+        }
+
+    }
+    [SerializeField]
+    private Image Yellow;
+    [SerializeField]
+    private Image Black;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleGauge();
+    }
+    private void HandleGauge()
+    {
+       // if (fillAmount != Yellow.fillAmount)
+      //  {
+            Yellow.fillAmount = fillAmount;
+            Black.fillAmount = fillAmountBlack;
+
+      //  }
+
+    }
+    private float Map(float value, float maxFuel)
+    {
+
+        return value / maxFuel;
+    }
+
+}
+
+
+
